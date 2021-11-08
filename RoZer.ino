6d0a2e7321aabca484c6b1b478ec40ce1d2316a2
joashcabanilla <joash0199@gmail.com>
2021-11-09 01:37:12 +0800

    room mapping update 2

--- RoZer.ino
+++ RoZer.ino
@@ -1,12 +1,13 @@
 #include <AFMotor.h>
+#include <EEPROM.h>
 #define bt_serial Serial1
 AF_DCMotor motor1(3);
 AF_DCMotor motor2(4);
 
 char command = ' ';
 char mapping = 'N';
-char mapp[5000];  
-int mapp_len;
+int movement_num;
+int address;
 
 void setup() 
 {       
@@ -14,9 +15,8 @@ void setup()
   bt_serial.begin(9600);
   motor1.setSpeed(120);
   motor2.setSpeed(120);
-  Stop();
-  memset(mapp, 0, sizeof mapp);
-  mapp_len = 0;
+  EEPROM.read(0) != 0 ? Saved_mapping() : movement();  
+  address = 0;
 }
 
 void loop()
@@ -24,8 +24,16 @@ void loop()
   if(bt_serial.available())
   {
     command = bt_serial.read();
-    command == 'W' ? mapping = 'Y' : command == 'w' ? mapping = 'N': ' ';
-    command != 'D' ? mapping == 'Y' ? Mapping(): movement():Disconnected();
+    Serial.println(command);
+    if(command != 'D')
+    {
+      command == 'W' ? mapping = 'Y' : command == 'w' ? mapping = 'N': command == 'X' ? mapping = 'C': command == 'V' ? mapping = 'S' : ' ';
+      mapping == 'C' ? Clear_mapping() : mapping == 'Y' ? Mapping() : mapping == 'S' ? Start_cleaning() : movement();
+    }
+    else
+    {
+      Disconnected();
+    }
   }
 }
 
@@ -60,42 +68,84 @@ void forward()
 {
   motor1.run(FORWARD);
   motor2.run(FORWARD);
-
+  movement_num = 1;
 }
 
 void backward()
 {
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
+  movement_num = 2;
 }
 
 void left()
 {
   motor1.run(BACKWARD);
   motor2.run(FORWARD); 
+  movement_num = 3;
 }
 
 void right()
 {
   motor1.run(FORWARD);
   motor2.run(BACKWARD);
+  movement_num = 4;
 } 
 
 void Stop()
 {
   motor1.run(RELEASE);
   motor2.run(RELEASE);
+  movement_num = 5;
 }
 
 void Disconnected()
 {
   Stop();
-  memset(mapp, 0, sizeof mapp);
 }
 
 void Mapping()
 {
   movement();
-  mapp_len = sizeof(mapp) / sizeof(char);
-  mapp[mapp_len+1] = command;
+  if(movement_num != 5)
+  {
+    EEPROM.write(address,movement_num);
+    address++;
+  }
+}
+
+void Start_cleaning()
+{
+   for (int i = 0 ; i < EEPROM.length() ; i++) {
+    switch(EEPROM.read(i))
+    {
+      case 1: 
+        forward();
+        break;
+        
+      case 2: 
+        backward();
+        break;
+        
+      case 3: 
+        left();
+        break;
+        
+      case 4: 
+        right();
+        break;
+    }
+  }
+}
+
+void Saved_mapping()
+{
+  Serial.print("MAY NAKA SAVED NA ROOM MAPPING");
+   //send data in bt module
+} 
+void Clear_mapping()
+{
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
 }
