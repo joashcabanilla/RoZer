15ba035d965b9dc2a1bec38bbf3d02b81553abe4
joashcabanilla <joash0199@gmail.com>
2021-11-28 21:53:27 +0800

    new update room mapping

--- RoZer.ino
+++ RoZer.ino
@@ -10,31 +10,22 @@ char command = ' ';
 char mapping = ' ';
 
 int seq = 0;
-int seq_Array[2500];
-
-int fwd_Counter = -1;
-int lft_Counter = -1;
-int rgt_Counter = -1;
-int bwd_Counter = -1;
-int stp_Counter = -1;
-
-unsigned long int current_Time0 = 0;
-unsigned long int current_Time1 = 0;
-unsigned long int current_Time2 = 0;
-unsigned long int current_Time3 = 0;
-unsigned long int current_Time4 = 0;
-
-unsigned long int total_Fwd_Time[10];
-unsigned long int total_Lft_Time[10];
-unsigned long int total_Rgt_Time[10];
-unsigned long int total_Bwd_Time[10];
-unsigned long int total_Stp_Time[10];
-
+byte seq_movement = 0;
+float delay_time = 0.0000f;
+float current_time;
+float stop_time;
 
 void setup() 
 {       
   Serial.begin(9600);
   bt_serial.begin(9600);
+
+  //TESTING CLEAR MAPPING-----------------------------------------------------------------------
+  for (int i = 0 ; i < EEPROM.length(); i++) {
+    EEPROM.write(i, 0);
+  }
+  //----------------------------------------------------------------------------------------------
+  
   EEPROM.read(0) != 0 ? Saved_mapping() : movement();  
 }
 
@@ -43,7 +34,7 @@ void loop()
   if(bt_serial.available())
   {
     command = bt_serial.read();
-    Serial.println(command);
+    //Serial.println(command);
     if(command != 'D')
     {
       command == 'M' ? mapping = 'Y' : command == 'E' ? mapping = 'N': command == 'A' ? mapping = 'C': command == 'C' ? mapping = 'S' : command == 'T' ? mapping = 'Q' : ' ';
@@ -62,6 +53,7 @@ void movement()
   motor2.setSpeed(200);
   motor3.setSpeed(200);
   motor4.setSpeed(200);
+  Stop();
   switch(command){
     case 'F':  
       forward();
@@ -75,9 +67,7 @@ void movement()
     case 'R':
       right();
       break;
-    case 'S':
-      Stop();
-    }
+  }
 }
 void forward()
 {
@@ -126,59 +116,40 @@ void Disconnected()
 
 void Mapping()
 {
-  motor1.setSpeed(200);
-  motor2.setSpeed(200);
-  motor3.setSpeed(200);
-  motor4.setSpeed(200);
+  Serial.println(command);
   switch(command){
     case 'F':  
-      go_Forward();
+      forward();
+      seq_movement = 1;
+      current_time = millis();
+      Serial.println(current_time, 4);
       break;
     case 'B':  
-       go_Backward();
+       backward();
+       seq_movement = 2;
       break;
     case 'L':  
-      go_Left();
+      left();
+      seq_movement = 3;
       break;
     case 'R':
-      go_Right();
+      right();
+      seq_movement = 4;
       break;
     case 'S':
-      go_Stop();
+      Stop();
+      stop_time = millis();
+      Serial.println(stop_time, 4);
+      delay_time = (stop_time - current_time) / 1000;
+      Serial.println(delay_time, 4);
+      EEPROM.write(0,delay_time);
+      Serial.println(EEPROM.read(0),4);
       break;
-    }
+  }
 }
 
 void Start_cleaning()
 {
-    int x = 0;
-   for(int z=3000;z < EEPROM.length();z++)
-   {
-     x = x + EEPROM.read(z);
-   }
-   
-   for(int i=0; i<x+1; i++){
-    byte r = EEPROM.read(2*i);
-    Serial.println(r);
-    switch(r){
-      case 1:
-        forward();
-        break;
-      case 2:
-        left();
-        break;
-      case 3:
-        right();
-        break;
-      case 4:
-        backward();
-        break; 
-      case 5:
-        Stop();
-        break;                          
-      }
-     delay((EEPROM.read(i+1))*1000);
-    }
 }
 
 void Stop_cleaning()
@@ -194,222 +165,12 @@ void Saved_mapping()
 
 void Clear_mapping()
 {
-  fwd_Counter = -1;
-  lft_Counter = -1;
-  rgt_Counter = -1;
-  bwd_Counter = - 1;
-  stp_Counter = - 1;
-  
-  for (int i = 0; i < 10; i++) {
-    total_Fwd_Time[i] = 0;
-    total_Lft_Time[i] = 0;
-    total_Rgt_Time[i] = 0;
-    total_Bwd_Time[i] = 0;
-    total_Stp_Time[i] = 0;
-  }
-    for (int i = 0; i < 2500; i++) {
-    seq_Array[i] = 0;
-  }
-
-  seq = 0;
   for (int i = 0 ; i < EEPROM.length(); i++) {
     EEPROM.write(i, 0);
   }
 }
 
-void go_Forward() {
-  forward();
-
-  current_Time0 = millis();
-  int i = seq_Array[(seq - 1)];
-  switch (i) {
-    case 2: 
-      total_Lft_Time[lft_Counter + 1] = (current_Time0 - current_Time1);
-      lft_Counter++;
-      break;
-
-    case 3: 
-      total_Rgt_Time[rgt_Counter + 1] = (current_Time0 - current_Time2);
-      rgt_Counter++;
-      break;
-
-    case 4:
-      total_Bwd_Time[bwd_Counter + 1] = (current_Time0 - current_Time3);
-      bwd_Counter++;
-      break;
-
-    case 5:
-      total_Stp_Time[stp_Counter + 1] = (current_Time0 - current_Time4);
-      stp_Counter++;
-      break;
-  }
-
-  seq_Array[seq] = 1;
-  seq++;
-}
-
-void go_Left() {
-  left();
-
-  current_Time1 = millis();
-  int i = seq_Array[(seq - 1)];
-  switch (i) {
-    case 1:
-      total_Fwd_Time[fwd_Counter + 1] = (current_Time1 - current_Time0);
-      fwd_Counter++;
-      break;
-
-    case 3:
-      total_Rgt_Time[rgt_Counter + 1] = (current_Time1 - current_Time2);
-      rgt_Counter++;
-      break;
-
-    case 4:
-      total_Bwd_Time[bwd_Counter + 1] = (current_Time1 - current_Time3);
-      bwd_Counter++;
-      break;
-
-    case 5:
-      total_Stp_Time[stp_Counter + 1] = (current_Time1 - current_Time4);
-      stp_Counter++;
-      break;
-  }
-
-  seq_Array[seq] = 2;
-  seq++;
-}
-
-void go_Right() {
-  right();
-
-  current_Time2 = millis();
-  int i = seq_Array[(seq - 1)];
-  switch (i) {
-    case 1:
-      total_Fwd_Time[fwd_Counter + 1] = (current_Time2 - current_Time0);
-      fwd_Counter++;
-      break;
-
-    case 2:
-      total_Lft_Time[lft_Counter + 1] = (current_Time2 - current_Time1);
-      lft_Counter++;
-      break;
-
-    case 4:
-      total_Bwd_Time[bwd_Counter + 1] = (current_Time2 - current_Time3);
-      bwd_Counter++;
-      break;
-
-    case 5:
-      total_Stp_Time[stp_Counter + 1] = (current_Time2 - current_Time4);
-      stp_Counter++;
-      break;
-  }
-
-  seq_Array[seq] = 3;
-  seq++;
-}
-
-void go_Backward() {
-  backward();
-
-  current_Time3 = millis();
-  int i = seq_Array[(seq - 1)];
-  switch (i) {
-    case 1:
-      total_Fwd_Time[fwd_Counter + 1] = (current_Time3 - current_Time0);
-      fwd_Counter++;
-      break;
-
-    case 2:
-      total_Lft_Time[lft_Counter + 1] = (current_Time3 - current_Time1);
-      lft_Counter++;
-      break;
-
-    case 3:
-      total_Rgt_Time[rgt_Counter + 1] = (current_Time3 - current_Time2);
-      rgt_Counter++;
-      break;
-
-    case 5:
-      total_Stp_Time[stp_Counter + 1] = (current_Time3 - current_Time4);
-      stp_Counter++;
-      break;
-  }
-
-  seq_Array[seq] = 4;
-  seq++;
-}
-
-void go_Stop() {
-  Stop();
-
-  current_Time4 = millis();
-  int i = seq_Array[(seq - 1)];
-  switch (i) {
-    case 1:
-      total_Fwd_Time[fwd_Counter + 1] = (current_Time4 - current_Time0);
-      fwd_Counter++;
-      break;
-
-    case 2:
-      total_Lft_Time[lft_Counter + 1] = (current_Time4 - current_Time1);
-      lft_Counter++;
-      break;
-
-    case 3:
-      total_Rgt_Time[rgt_Counter + 1] = (current_Time4 - current_Time2);
-      rgt_Counter++;
-      break;
-
-    case 4:
-      total_Bwd_Time[bwd_Counter + 1] = (current_Time4 - current_Time3);
-      bwd_Counter++;
-      break;
-  }
-
-  seq_Array[seq] = 5;
-  seq++;
-}
-
 void save_mapping()
 { 
-  int Total_seq = seq / 255;
-  for(int x=0; x < Total_seq; x++)
-  {
-    EEPROM.write(3000+x,255);
-  }
-
-  EEPROM.write(3000+Total_seq,seq -(Total_seq*255));
   
-  for(int i=0; i<seq; i++){ 
-    EEPROM.write(2*i,seq_Array[i]);
-  }
-
-  for(int i=1; i<seq+1; i++){           
-  if(seq_Array[i-1]==1){
-    static byte a=0;
-    EEPROM.write(2*i-1,(total_Fwd_Time[a])/1000);
-    }
-  else if(seq_Array[i-1]==2){
-    static byte b=0;
-    EEPROM.write(2*i-1,(total_Lft_Time[b])/1000);
-    b++;
-    }
-  else if(seq_Array[i-1]==3){
-    static byte c=0;
-    EEPROM.write(2*i-1,(total_Rgt_Time[c])/1000);
-    c++;
-    }
-  else if(seq_Array[i-1]==4){
-    static byte d=0;
-    EEPROM.write(2*i-1,(total_Bwd_Time[d])/1000);  
-    d++;
-    }
-  else if(seq_Array[i-1]==5){
-    static byte e=0;
-    EEPROM.write(2*i-1,(total_Stp_Time[e])/1000);  
-    e++;
-    }             
-  }
 }
